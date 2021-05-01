fd43a545fe051956205b39441b0a30725f918945
KISHI Kousuke <ksh2ksk4@gmail.com>
2021-05-02 08:32:32 +0900

    Configuration

--- public/index.php
+++ public/index.php
@@ -2,10 +2,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Middleware\ContentLengthMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->add(new ContentLengthMiddleware());
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('Hello World');

b5e7314716ecd961e5fa491e28914a8428375587
KISHI Kousuke <ksh2ksk4@gmail.com>
2021-05-02 09:52:36 +0900

    Add dependencies - Monolog

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -6,12 +7,35 @@ use Slim\Middleware\ContentLengthMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
+
 $app->add(new ContentLengthMiddleware());
 $app->addErrorMiddleware(true, true, true);
 
+$container->set('myLogger', function () {
+    $logger = new \Monolog\Logger('myLogger');
+    $logger->pushHandler(
+        new \Monolog\Handler\StreamHandler(__DIR__ . '/../logs/app.log')
+    );
+
+    return $logger;
+});
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello World');
+    if (!$this->has('myLogger')) {
+        throw new RuntimeException('No myLogger');
+    }
+
+    $message = 'Hello World';
+
+    $logger = $this->get('myLogger');
+    $logger->info($message);
+
+    $response->getBody()->write($message);
+
     return $response;
 });
 
